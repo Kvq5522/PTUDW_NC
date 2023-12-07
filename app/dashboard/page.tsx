8ff4a85e43fcca03c2a0f4cb4ab6ec80ef1e0ad3
unknown <vinhkhangquach2002@gmail.com>
2023-12-07 17:39:20 +0700

    fix deploy error

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -75,7 +75,7 @@ const Dashboard = () => {
     );
 
   return (
-    <div className={cn("overflow-x-hidden px-8 p-4", className)}>
+    <div className={cn("overflow-x-hidden px-8 p-4")}>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
         {classList?.map((classroom: any, index) => {
           return (
