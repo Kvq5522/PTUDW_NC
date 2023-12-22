603ae6749714520c8c36b4b5fe26295e0aec1898
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-12-22 13:24:41 +0700

    update page.tsx stream detail

--- app/classroom/[classroomId]/(routes)/stream/[streamitemId]/stream-detail/page.tsx
+++ app/classroom/[classroomId]/(routes)/stream/[streamitemId]/stream-detail/page.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "@/Styles/stream-detail.css";
 
 import { RiFileListLine } from "react-icons/ri";
 import { GrScorecard } from "react-icons/gr";
-import { MoreVertical } from "lucide-react";
+import { MoreVertical, Dot } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
+import { Button } from "@/components/ui/button";
+import CommentArea from "@/components/CommentArea";
 
 interface streamDetailProps {
   classID: string;
@@ -14,6 +17,9 @@ interface streamDetailProps {
 }
 
 const StreamContentDetail = (props: streamDetailProps) => {
+  const [description, setDescription] = useState("");
+
+  const [files, setFiles] = useState([]);
   const itemIcon =
     props.itemType === "grade-material" ? (
       <GrScorecard size={20} color="white" />
@@ -26,14 +32,40 @@ const StreamContentDetail = (props: streamDetailProps) => {
       <div className="stream-detail-icon">
         <Avatar className="h-[36px] w-[36px]">
           <AvatarImage className="object-cover" />
-          <AvatarFallback className=" bg-[#3e9e3e]">{itemIcon}</AvatarFallback>
+          <AvatarFallback className="bg-[#3e9e3e]">{itemIcon}</AvatarFallback>
         </Avatar>
-        ;
       </div>
       <div className="stream-detail-main">
-        <div className="stream-detail-content"></div>
+        <div className="stream-detail-content">
+          <div className="stream-detail-header">
+            <div className="stream-detail-label">
+              <span>GA01 - Authentication (Dealine 10pm Jan 04)</span>
+              <div className="detail-actions">
+                <Button variant="ghost" size="icon" className="rounded-full">
+                  <MoreVertical />
+                </Button>
+              </div>
+            </div>
+            <div className="stream-detail-sublabel">
+              <div className="detail-creator">Lưu Minh Phát</div>
+              <div className="mx-[0.25rem]">•</div>
+              <div className="detail-daytime">Dec 21</div>
+            </div>
+          </div>
+          {description === "" ? (
+            <></>
+          ) : (
+            <>
+              <div className="stream-detail-description">
+                <span>{description}</span>
+              </div>
+            </>
+          )}
+        </div>
         <div className="stream-detail-files"></div>
-        <div className="stream-detail-comment"></div>
+        <div className="stream-detail-comment">
+          <CommentArea />
+        </div>
       </div>
     </div>
   );
