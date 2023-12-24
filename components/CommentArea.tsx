dc9b33d59fb21064111de8a64c423e5e4a5aa7b9
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-12-24 16:41:20 +0700

    update comment v3.0

--- components/CommentArea.tsx
+++ components/CommentArea.tsx
@@ -1,15 +1,51 @@
 import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
 import { Send, Users } from "lucide-react";
-import { Input } from "./ui/input";
 
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
 import { Textarea } from "./ui/textarea";
+import { Bold, Italic, Underline } from "lucide-react";
+
+const formSchema = z.object({
+  commentId: z.string(),
+  creatorId: z.string(),
+  streamitemId: z.string(),
+  classId: z.string(),
+  comment: z.string(),
+});
 
 const CommentArea = () => {
   const [dmCount, setDmCount] = useState();
   const [commentItems, setCommentItems] = useState([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      commentId: "",
+      creatorId: "",
+      streamitemId: "",
+      classId: "",
+      comment: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
 
   return (
     <div className="comment-container">
@@ -19,28 +55,51 @@ const CommentArea = () => {
         class comment
       </div>
 
-      {commentItems.length === 0 ? (
+      {/* {commentItems.length === 0 ? (
         <></>
       ) : (
         <>
           <div className="comment-show"></div>
         </>
-      )}
+      )} */}
       <div className="comment-show"></div>
 
       <div className="comment-box">
-        <div className="comment-box-wrapper relative">
-          <Avatar className="h-[2rem] w-[2rem] cmtbavt top-5">
-            <AvatarImage className="object-cover" />
-            <AvatarFallback className=" bg-[#3e9e3e]"></AvatarFallback>
-          </Avatar>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="comment-box-wrapper"
+          >
+            <Avatar className="h-[2rem] w-[2rem] cmtbavt">
+              <AvatarImage className="object-cover" />
+              <AvatarFallback className="bg-[#3e9e3e]"></AvatarFallback>
+            </Avatar>
+            <FormField
+              control={form.control}
+              name="comment"
+              render={({ field }) => (
+                <FormItem className="comment-chatbox">
+                  <FormControl>
+                    <Textarea
+                      placeholder="Add class comment"
+                      className="chatbox resize-none"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
 
-          <Textarea className="comment-chatbox" />
-
-          <Button variant="ghost" size="icon" className="chatbox-send top-[-1.5rem]">
-            <Send />
-          </Button>
-        </div>
+            <Button
+              type="submit"
+              variant="ghost"
+              size="icon"
+              className="chatbox-send top-[-1.5rem]"
+            >
+              <Send />
+            </Button>
+          </form>
+        </Form>
       </div>
     </div>
   );
